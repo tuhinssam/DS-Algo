49aa3a65012e112f37e798c0dd7e940e5d07f9f2
Tuhin Subhra Samanta <tuhinssam@gmail.com>
2022-01-14 12:26:35 +0530

    Update 8_CheckArrayIsSorted.java

--- Array/8_CheckArrayIsSorted.java
+++ Array/8_CheckArrayIsSorted.java
@@ -6,10 +6,10 @@ class GFG {
 	public static void main (String[] args) {
 		System.out.println("GfG!");
 		int[] arr = {-5, 8, 9, 10, 15};
-		String result = maxKSubarrayBF(arr);
+		String result = isArraySorted(arr);
 		System.out.println(result);
 	}
-    static String maxKSubarrayBF(int[] arr){
+    static String isArraySorted(int[] arr){
         for(int i = 1; i< arr.length; i++){
             if (arr[i-1]>arr[i])
                return "not sorted";
